8ad836114bc20a75ca6b9178aa6046328bd9edfd
huanghaifen <wstkzc@gmail.com>
2017-07-26 16:42:17 +0800

    Site updated: 2017-07-26 16:42:14

--- js/src/girls.js
+++ js/src/girls.js
@@ -67,65 +67,6 @@ jQuery(document).ready(function ($) {
 //	4000);
 });
 
-//鼠标在某些元素上方时
-jQuery(document).ready(function ($) {
-    $('h2 a').click(function () {//标题被点击时
-        showMessage('正在用吃奶的劲加载《<span style="color:#0099cc;">' + $(this).text() + '</span>》请稍候');
-    });
-    $('h2 a').mouseover(function () {
-        showMessage('要看看《<span style="color:#0099cc;">' + $(this).text() + '</span>》这篇文章么？');
-    });
-    $('#prev-page').mouseover(function(){
-        showMessage('要翻到上一页吗?');
-    });
-    $('#next-page').mouseover(function(){
-        showMessage('要翻到下一页吗?');
-    });
-    $('#index-links li a').mouseover(function () {
-        showMessage('去 <span style="color:#0099cc;">' + $(this).text() + '</span> 逛逛');
-    });
-    $('.comments').mouseover(function () {
-        showMessage('<span style="color:#0099cc;">' + visitor + '</span> 向评论栏出发吧！');
-    });
-    $('#submit').mouseover(function () {
-        showMessage('确认提交了么？');
-    });
-    $('#s').focus(function () {
-        showMessage('输入你想搜索的关键词再按Enter(回车)键就可以搜索啦!');
-    });
-    $('#go-prev').mouseover(function () {
-        showMessage('点它可以后退哦！');
-    });
-    $('#go-next').mouseover(function () {
-        showMessage('点它可以前进哦！');
-    });
-    $('#refresh').mouseover(function () {
-        showMessage('点它可以重新载入此页哦！');
-    });
-    $('#go-home').mouseover(function () {
-        showMessage('点它就可以回到首页啦！');
-    });
-    $('#addfav').mouseover(function () {
-        showMessage('点它可以把此页加入书签哦！');
-    });
-    $('#nav-two a').mouseover(function () {
-        showMessage('嘘，从这里可以进入控制面板的哦！');
-    });
-    $('.post-category a').mouseover(function () {
-        showMessage('点击查看此分类下得所有文章');
-    });
-    $('.post-heat a').mouseover(function () {
-        showMessage('点它可以直接跳到评论列表处.');
-    });
-    $('#tho-shareto span a').mouseover(function () {
-        showMessage('你知道吗?点它可以分享本文到'+$(this).attr('title'));
-    });
-    $('#switch-to-wap').mouseover(function(){
-        showMessage('点击可以切换到手机版博客版面');
-    });
-});
-
-
 //无聊讲点什么
 jQuery(document).ready(function ($) {
 
@@ -153,55 +94,6 @@ jQuery(document).ready(function ($) {
 			    complete: showMessage(msgs[i])
 			});
     }, 45000);
-});
-
-//评论资料
-jQuery(document).ready(function ($) {
-    $("#author").click(function () {
-        showMessage("留下你的尊姓大名！");
-        $(".spig").animate({
-            top: $("#author").offset().top - 70,
-            left: $("#author").offset().left - 170
-        },
-		{
-		    queue: false,
-		    duration: 1000
-		});
-    });
-    $("#email").click(function () {
-        showMessage("留下你的邮箱，不然你的头像是小怪物喽！");
-        $(".spig").animate({
-            top: $("#email").offset().top - 70,
-            left: $("#email").offset().left - 170
-        },
-		{
-		    queue: false,
-		    duration: 1000
-		});
-    });
-    $("#url").click(function () {
-
-        showMessage("快快告诉我你的家在哪里，好让我去参观参观！");
-        $(".spig").animate({
-            top: $("#url").offset().top - 70,
-            left: $("#url").offset().left - 170
-        },
-		{
-		    queue: false,
-		    duration: 1000
-		});
-    });
-    $("#comment").click(function () {
-        showMessage("认真填写哦！不然会被认作垃圾评论的！我的乖乖~");
-        $(".spig").animate({
-            top: $("#comment").offset().top - 70,
-            left: $("#comment").offset().left - 170
-        },
-		{
-		    queue: false,
-		    duration: 1000
-		});
-    });
 });
 
 var spig_top = 50;
